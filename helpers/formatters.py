89f808f5f51ccb43993baf2bec4b64c064db82d6
mabhishek7081 <mabhishek7081@gmail.com>
2023-11-08 23:37:05 +0530

    Removed unused code

--- helpers/formatters.py
+++ helpers/formatters.py
@@ -17,7 +17,6 @@ def convert_to_qna_chucks(req_data):
             arr.append(output)
     return arr
 
-
 def merge_duplicates(dataArr):
     unique_questions = {}
     for item in dataArr:
@@ -36,33 +35,6 @@ def merge_duplicates(dataArr):
 
     # Convert the dictionary values back to a list
     return [{"question": k, **v} for k, v in unique_questions.items()]
-
-
-# def merge_duplicate_questions_with_answer_data(dataArr):
-#     unique_questions = {}
-#     for item in dataArr:
-#         question = item.get("question", "")
-#         answer = item.get("answer")
-#         vote = item.get("answer_vote")
-
-#         if question not in unique_questions:
-#             unique_questions[question] = {
-#                 "answer": [{"answer": answer, "vote": vote}] if answer is not None and answer != "" else [],
-#                 "unanswered": item.get("unanswered", False),
-#                 "times_asked": 1
-#             }
-#         else:
-#             if answer is not None and answer != "":
-#                 unique_questions[question]["answer"].append({
-#                     "answer": answer,
-#                     "vote": vote
-#                 })
-#             unique_questions[question]["times_asked"] += 1
-
-#     # Convert the dictionary values back to a list
-#     merged_data = [{"question": k, **v} for k, v in unique_questions.items()]
-
-#     return merged_data
 
 # get top questions by times asked, sorted in descending order
 def get_top_questions_by_times_asked(qna_list, top_n=10):
